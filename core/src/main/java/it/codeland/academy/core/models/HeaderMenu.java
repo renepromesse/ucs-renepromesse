9c5c4df2f5c848b35595abbd1a99eb178703eaf3
Rene Promesse <nrenejordan1@gmail.com>
2021-12-31 13:06:33 +0200

    added the social media

---
+++ core/src/main/java/it/codeland/academy/core/models/HeaderMenu.java
@@ -0,0 +1,74 @@
+package it.codeland.academy.core.models;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.settings.SlingSettingsService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+import java.util.*;
+
+@Model(adaptables = Resource.class)
+public class HeaderMenu {
+    private static final Logger LOG = LoggerFactory.getLogger(HeaderMenu.class);
+
+    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
+    @Default(values="No resourceType")
+    protected String resourceType;
+
+    @OSGiService
+    private SlingSettingsService settings;
+    @SlingObject
+    private Resource currentResource;
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    private String message;
+
+    @PostConstruct
+    protected void init() {
+        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+        String currentPagePath = Optional.ofNullable(pageManager)
+                .map(pm -> pm.getContainingPage(currentResource))
+                .map(Page::getPath).orElse("");
+
+        message = "Hello World!\n"
+            + "Resource type is: " + resourceType + "\n"
+            + "Current page is:  " + currentPagePath + "\n"
+            + "This is instance: " + settings.getSlingId() + "\n";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public Iterator<Page> getChildPages() {
+        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+        String currentPagePath = Optional.ofNullable(pageManager)
+                .map(pm -> pm.getContainingPage(currentResource))
+                .map(Page::getPath).orElse("");
+
+        try {
+            Page rootPage = pageManager.getPage("/content/ucs-exercise-renepromesse/us/en/uni-credit");
+            Iterator<Page> childPageIterator = rootPage.listChildren();
+            return childPageIterator;
+        } catch (Exception e) {
+            LOG.info("\n Error from sub Items  {} ", e.getMessage());
+            return null;
+        }
+    }
+
+}
